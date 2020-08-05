9aa3757bdfd937241f7c40ed7cc3273ed5fa1efc
Liam Chipman <chipman.lian@gmail.com>
2020-08-04 22:41:27 -0600

    Actual Initial Commit

---
+++ character_creator_pathfinder2e_v1/src/liam/chipman/Main.java
@@ -0,0 +1,12 @@
+package liam.chipman;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Pathfinder Character Creator!");
+        System.out.println("Let's get started!");
+    }
+    public static void getAbilityScores(){
+
+    }
+}
